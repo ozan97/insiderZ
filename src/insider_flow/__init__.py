005cd2f6c3ef11b2baefa12cff7b6e0155d2d2a7
Ozan <ozan.agtas@home.ch>
2026-01-16 18:53:30 +0100

    add scheduled run

--- src/insider_flow/__init__.py
+++ src/insider_flow/__init__.py
@@ -1,6 +1,12 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import (
+    Definitions, 
+    load_assets_from_modules, 
+    define_asset_job, 
+    build_schedule_from_partitioned_job
+)
 from .resources import SECClient
-from . import assets
+import assets
+
 
 all_assets = load_assets_from_modules([
     assets.ingestion, 
@@ -11,8 +17,21 @@ all_assets = load_assets_from_modules([
 ])
 
 
+daily_update_job = define_asset_job(
+    name="daily_update_job", 
+    selection=all_assets
+)
+daily_schedule = build_schedule_from_partitioned_job(
+    job=daily_update_job,
+    cron_schedule="0 9 * * 1-5", 
+    description="Daily Morning Run for Insider Trades",
+    execution_timezone="Europe/Zurich"
+)
+
 defs = Definitions(
     assets=all_assets,
+    jobs=[daily_update_job],
+    schedules=[daily_schedule],
     resources={
         "sec_client": SECClient(),
     },
